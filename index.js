48ff23f8bcafe137f689940a1e0ad1e6ea9eb60d
hytham Alobydi <hythamalobydi@hythams-MacBook-Pro.local>
2017-01-18 20:53:32 -0500

    chron: Implment snowboy

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // Declerations
-const config=require('fs');
+const fs=require('fs');
 const record=require('node-record-lpcm16');
 const request=require('request');
 const snowboy_det=require('snowboy').Detector;
 const snowboy_mod=require('snowboy').Models;
 //const witai=require('node-wit');
-const mqtt=require('mqtt');
+//const mqtt=require('mqtt');
 
 // Read the configuration file from /etc/witiot.json
-var config_cont=fs.readFile('/etc/witiot.json').toString();
-var config_json=JSON.parse(config_cont);
 
-var witToken=wit.Token; // Add wit tokentoS
+
+var config_json=JSON.parse(fs.readFileSync('resources/witiot.json', 'utf8'));//fs.readFile("resources//witiot.json").toString();
+//var config_json=JSON.parse(config_cont);
+
+//var witToken=wit.Token; // Add wit tokentoS
 
 
 exports.parseResult =function (err,resp,body){
@@ -21,37 +23,39 @@ exports.parseResult =function (err,resp,body){
 // Setting up snowboy
 const models=new snowboy_mod();
 models.add({
-    file:'resources/snowbody.umdl',
-    sesnsitivity:'0.5',
-    hotwords:'snowboy'
+    file:config_json.snowboy.file,
+    sesnsitivity:config_json.snowboy.sensativity,
+    hotwords:config_json.snowboy.hotword
 });
 
 // Setting up Detector
-const det=new Detector({
-    resources:'resources/common.res',
+const det=new snowboy_det({
+    resource:"resources/common.res",
     models:models,
     audioGain:2.0
 });
 
-det.on('sillence',function(){
-    // Detect Scleince
+det.on('silence', function () {
+  console.log('silence');
 });
 
-det.on('sound',function(){
-    // On Sound detected
+det.on('sound', function () {
+  console.log('sound');
 });
 
-det.on('hotwords',function(){
-    // On Hotword Detected
-    // play a beep
+det.on('error', function () {
+  console.log('error');
+});
 
-   
-})
+det.on('hotword', function (index, hotword) {
+  console.log('hotword', index, hotword);
+});
 
 
 record.start({
     verbos:true,
-    recordProgram:'arecord'
+    threshold:0,
+    recordProgram:'rec'
 }).pipe(det);
 
 
