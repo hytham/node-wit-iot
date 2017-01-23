05a4f02e297e4b6e441e37b1112d3110d2beae95
hytham Alobydi <hythamalobydi@hythams-MacBook-Pro.local>
2017-01-22 20:59:56 -0500

    chron: add code state diagram

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Declerations
 const fs=require('fs');
+
 const beep=require('beeper')
 const record=require('node-record-lpcm16');
 const Polly = require('aws-sdk').Polly;
@@ -10,74 +11,80 @@ const snowboy_mod=require('snowboy').Models;
 
 var isRecording=false;
 var rec;
-//const witai=require('node-wit');
-//const mqtt=require('mqtt');
+var current_state;
+var sclance_count=20; // counter to see the sclience count befor the transition happen
+var config_json;
 
-// Read the configuration file from /etc/witiot.json
+exports.init=function(){
+ config_json
+    =JSON.parse(fs.readFileSync('resources/witiot.json', 'utf8'));/
+
+    var models=new snowboy_mod();
+    models.add({
+        file:config_json.snowboy.file,
+        sesnsitivity:config_json.snowboy.sensativity,
+        hotwords:config_json.snowboy.hotword
+    });
+
+    var det=new snowboy_det({
+        resource:"resources/common.res",
+        models:models,
+        audioGain:parseFloat(config_json.snowboy.audiogain)
+    });
 
 
-var config_json=JSON.parse(fs.readFileSync('resources/witiot.json', 'utf8'));//fs.readFile("resources//witiot.json").toString();
-//var config_json=JSON.parse(config_cont);
+    det.on('silence', function () {
+        console.log('silence');
+        RunFSM();
+    });
 
-//var witToken=wit.Token; // Add wit tokentoS
+    det.on('sound', function () {
+        console.log('sound');
+        RunFSM();
+    });
+
+    det.on('error', function () {
+        console.log('error');
+        RunFSM();
+    });
+
+    det.on('hotword', function (index, hotword) {
+
+        console.log('hotword', index, hotword);
+        isRecording=false;
+        beep();
+        StartRecordingSpeech();
+        RunFSM();
+    });
+    RunFSM();
+}
 
 
 exports.parseResult =function (err,resp,body){
     // Exposing the result of the respons to the outside
+    RunFSM();
 }
 
-// Setting up snowboy
-const models=new snowboy_mod();
-models.add({
-    file:config_json.snowboy.file,
-    sesnsitivity:config_json.snowboy.sensativity,
-    hotwords:config_json.snowboy.hotword
-});
-
-// Setting up Detector
-const det=new snowboy_det({
-    resource:"resources/common.res",
-    models:models,
-    audioGain:parseFloat(config_json.snowboy.audiogain)
-});
-
-det.on('silence', function () {
-  console.log('silence');
-  if(isRecording)
-    rec.stop();
-    isRecording=false;
-  
-});
-
-det.on('sound', function () {
-  console.log('sound');
-  
-});
-
-det.on('error', function () {
-  console.log('error');
-});
-
-det.on('hotword', function (index, hotword) {
-  console.log('hotword', index, hotword);
-  beep();
-  isRecording=false;
-  StartRecordingSpeech();
-});
 
 
-record.start({
-    verbos:true,
-    threshold:0,
-    recordProgram:'rec'
-}).pipe(det);
+exports.StartListenForHW=function(){
+    record.start({
+        verbos:true,
+        threshold:0,
+        recordProgram:'rec'
+    }).pipe(det);
+}
 
 exports.parseResult = function (err, resp, body) {
-  if (err) console.error(err)
-  console.log(body)
+  
+  if (err) 
+    console.error(err)
+  console.log(body);
+
+  RunFSM();
 }
 
-function StartRecordingSpeech(){
+exports.StartRecordingSpeech=function(){
     record.start({
         verbose: true,
         recordProgram: 'rec'
@@ -92,6 +99,25 @@ function StartRecordingSpeech(){
 
    
 }
+// Finite State Machines
+function RunFSM(){
+    // TODO Execute the FSM
+    evalTrans(); // Evaluate 
+    runStates(); // Run States 
+}
+function evalTrans(){
+    switch(current_state){
+
+    }
+
+}
+function runStates(){
+    switch(current_state){
+
+    }
+}
+
+
 
 
 
